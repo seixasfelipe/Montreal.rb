f3d3c8e748c45c3afd010e5be3d535a3461b995b
Alexandre Croteau-Pothier <alex@motioneleven.com>
2013-06-27 19:39:55 -0400

    new rails 4 installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MontrealRb::Application.config.secret_key_base = '888f1cf66f71f2910d248cd2cf56b6f05b6a783d46c38cf85c16420338c651faea46f5350522525d6dfa5d7369310846e70ec761332d084fd4b381623f4b3b62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MontrealRb::Application.config.session_store :cookie_store, key: '_montreal_rb_session'
